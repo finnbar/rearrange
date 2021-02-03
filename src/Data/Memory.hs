5bfd1b1590206dcde095c6ae994ac3cfca81879c
Finnbar Keating <finnjkeating@gmail.com>
2021-02-03 12:29:06 +0000

    Greatly simplified implementation.

--- src/Data/Memory.hs
+++ src/Data/Memory.hs
@@ -6,8 +6,8 @@ import Prelude hiding (Monad(..), reads)
 import qualified Prelude as P
 import GHC.TypeLits (Symbol, CmpSymbol)
 import Control.Effect
-import Control.Effect.State ((:!)(..), Eff(..), Reads(..), Writes(..), UnionS)
-import Data.Type.Set hiding (Nub)
+import Control.Effect.State ((:!)(..), Eff(..))
+import Data.Type.Set
 import Foreign.Storable
 import Foreign.Ptr
 import Foreign.C.Types
@@ -21,28 +21,38 @@ data MAddr (s :: Symbol) t where
 
 newtype Memory (s :: [*]) a = Mem { runMemory :: Set s -> IO a }
 
--- TODO: This isn't merging R and W into RW.
 type instance Cmp (MAddr s t :! e) (MAddr s' t' :! e') = CmpSymbol s s'
 
+-- Note: Currently it should be impossible to get RW, but I'm including the case anyway.
+type family Reads (s :: [*]) :: [*] where
+    Reads '[] = '[]
+    Reads ((MAddr s t :! R) ': xs) = (MAddr s t :! R) ': Reads xs
+    Reads ((MAddr s t :! RW) ': xs) = (MAddr s t :! R) ': Reads xs
+    Reads (x ': xs) = Reads xs
+
+type family Writes (s :: [*]) :: [*] where
+    Writes '[] = '[]
+    Writes ((MAddr s t :! W) ': xs) = (MAddr s t :! W) ': Writes xs
+    Writes ((MAddr s t :! RW) ': xs) = (MAddr s t :! W) ': Writes xs
+    Writes (x ': xs) = Writes xs
+
 instance Effect Memory where
-    type Inv Memory f g = (IsSet f, IsSet g, Split f g (UnionS f g))
+    type Inv Memory f g = (IsSet f, IsSet g, Split f g (Union f g))
     type Unit Memory = '[]
-    -- Plan: allow this union to do RW effects, but Topsort can fail for now.
-    type Plus Memory f g = UnionS f g
+    type Plus Memory f g = Union f g
 
     return x = Mem $ \Empty -> P.return x
     (Mem e) >>= k =
         Mem $ \fg -> let (f, g) = split fg
                       in e f P.>>= ((\fn -> fn g) . runMemory . k)
 
-reads :: forall s t. Storable t => Memory '[MAddr s t :! 'R] t
+reads :: forall s t. Storable t => Memory '[MAddr s t :! R] t
 reads = Mem $ \(Ext (Addr pt :! _) Empty) -> peek pt
 
-writes :: forall s t. Storable t => t -> Memory '[MAddr s t :! 'W] ()
+writes :: forall s t. Storable t => t -> Memory '[MAddr s t :! W] ()
 writes x = Mem $ \(Ext (Addr pt :! _) Empty) -> poke pt x
 
---fn :: Memory '[MAddr "in" CInt :! 'R, MAddr "out" CInt :! 'W] ()
 fn = do
     input <- reads @"in"
     let output = input + 2 :: CInt
-    writes @"in" output
+    writes @"out" output
